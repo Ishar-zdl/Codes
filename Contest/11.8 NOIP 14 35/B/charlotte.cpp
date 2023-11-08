0f17628e3a4bb2a8a27a5b84c04a819b17f63f1b
Ishar-zdl <a152336@163.com>
2023-11-08 21:51:48 +0800

    12

--- Contest/11.8 NOIP 14 35/B/charlotte.cpp
+++ Contest/11.8 NOIP 14 35/B/charlotte.cpp
@@ -2,7 +2,7 @@
  * @Author: Ishar-zdl 
  * @Date: 2023-11-08 09:37:57 
  * @Last Modified by: Ishar-zdl
- * @Last Modified time: 2023-11-08 09:49:28
+ * @Last Modified time: 2023-11-08 15:35:38
  */
 #include<bits/stdc++.h>
 inline int read(){
@@ -12,27 +12,33 @@ inline int read(){
     return x*f;
 }
 inline void CIN_sync(){
-    #ifdef ONLINE_JUDGE
+    // #ifdef ONLINE_JUDGE
         std::ios::sync_with_stdio(false);
         std::cin.tie(0),std::cout.tie(0);
-    #endif
+    // #endif
 }
-const int N=2e3+10;
-int n,head[N],tot,q[N],cnt,dis[N][N];
+const int N=2e6+10;
+int n,head[N],tot,q[N],cnt;
 char ch[N];
 struct edge{int v,next;}e[N*2];
 inline void add(int u,int v){e[++tot]={v,head[u]},head[u]=tot;}
-void dfs(int x,int fa,int root){
-    for(int i=head[x];i;i=e[i].next){
-        int y=e[i].v;
-        if(y!=fa)dis[root][y]=dis[root][x]+1,dfs(y,x,root);
-    }
-}
+// void dfs(int x,int fa,int root){
+//     for(int i=head[x];i;i=e[i].next){
+//         int y=e[i].v;
+//         if(y!=fa)dis[root][y]=dis[root][x]+1,dfs(y,x,root);
+//     }
+// }
 int main(){
-    CIN_sync();
-    std::cin>>n>>ch+1;
-    for(int i=1;i<=n;++i)if(ch[i]=='1')q[++cnt]=i;
-    for(int i=1,u,v;i<n;++i)std::cin>>u>>v,add(u,v),add(v,u);
-    for(int i=1;i<=cnt;++i)dfs(i,i,i);
+    freopen("charlotte.in","r",stdin),freopen("charlotte.out","w",stdout);
+    // CIN_sync();
+    while(scanf("%d",&n)!=-1){
+        // tot//=0;memset(head,0,sizeof(head));memset(e,0,sizeof(e));
+        // std::cin>>ch+1;
+        scanf("%s",ch+1);
+        // for(int i=1;i<=n;++i)if(ch[i]=='1')q[++cnt]=i;
+        for(int i=1,u,v;i<n;++i)scanf("%d%d",&u,&v); //,add(u,v),add(v,u);
+        // for(int i=1;i<=cnt;++i)dfs(i,i,i);
+        printf("-1\n");
+    }
     
 }
--- Practice/Permutation and Combination/HZOI E/E.cpp
+++ Practice/Permutation and Combination/HZOI E/E.cpp
@@ -2,7 +2,7 @@
  * @Author: Ishar-zdl 
  * @Date: 2023-11-08 07:10:12 
  * @Last Modified by: Ishar-zdl
- * @Last Modified time: 2023-11-08 11:01:08
+ * @Last Modified time: 2023-11-08 19:20:42
  */
 #include<bits/stdc++.h>
 #define int long long 
@@ -26,14 +26,16 @@ inline int qpow(int a,int b){
     return ans;
 }
 inline int C(int n,int m){if(m>n)return 0;return jc[n]*ny[m]%mod*ny[n-m]%mod;}
-inline int lucas(int n,int m){
-    if(m==1)return 1;
-    return C(n%mod,m%mod)*lucas(n/mod,m/mod);
-}
+// inline int lucas(int n,int m){
+//     if(m==1)return 1;
+//     return C(n%mod,m%mod)*lucas(n/mod,m/mod);
+// }
 int main(){
     n=read(),k=read();jc[0]=ny[0]=1;
     for(int i=1;i<=n;++i)jc[i]=jc[i-1]*i%mod;
     ny[n]=qpow(jc[n],mod-2);
     for(int i=n-1;i;--i)ny[i]=ny[i+1]*(i+1)%mod;
+    int base=C(n,k);int ans=base;
+    for(int i=1;i<)
 }
 //n中选k然后算方案数。
