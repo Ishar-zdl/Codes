13725fbed46e041e93e52fd0a7cf3ac919b57428
Ishar-zdl <a152336@163.com>
2023-11-13 18:45:39 +0800

    123

--- Contest/Luogu NOIP/A P9836 plant/A P9836 plant.cpp
+++ Contest/Luogu NOIP/A P9836 plant/A P9836 plant.cpp
@@ -2,9 +2,10 @@
  * @Author: Ishar-zdl 
  * @Date: 2023-11-13 16:59:50 
  * @Last Modified by: Ishar-zdl
- * @Last Modified time: 2023-11-13 17:02:23
+ * @Last Modified time: 2023-11-13 18:45:35
  */
 #include<bits/stdc++.h>
+#define int long long
 #define endl '\n'
 #define FRE freopen("in.in","r",stdin),freopen("out.out","w",stdout);
 inline int read(){
@@ -19,6 +20,31 @@ inline void CIN_sync(){
         std::cin.tie(0),std::cout.tie(0);
     #endif
 }
+const int N=1e4+10,mod=998244353;
+int p[1500],prim[N],n,tot,a[N],w;
+std::unordered_map<int,int>mp;
+bool vis[N];
+inline void get_prime(){
+    for(int i=2;i<=N-3;++i){
+        if(!vis[i])prim[++tot]=i,mp[i]=tot;
+        for(int j=1;j<=tot&&prim[j]*i<=N-3;++j){
+            vis[i*prim[j]]=true;
+            if(i%prim[j]==0)break;
+        }
+    }
+}
+inline void fact(int x){
+    int pos=x;
+    for(int i=2;i*i<=pos;++i){
+        int sd=mp[i];
+        while(x%i==0)p[sd]++,x/=i;
+    }
+    if(x!=1)p[mp[x]]++;
+}
 int main(){
-       
+    n=read();w=read();get_prime();
+    for(int i=1;i<=n;++i)a[i]=read();
+    // fact
+    // for(int i=2;i*i<)
+    // for(int i=1;i<=N-3;++i)a[i]=read();
 }
